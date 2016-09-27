ef384e28eb40bfe51cab3ba21e4d9eef9f7446ee
Jonathan Neeskens <jonathan.neeskens@profects.com>
2016-09-27 09:15:16 +0200

    Created design of the menu

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title>Bon Temps</title>
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/lemonade.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+    <body>
+        <div class="menu bit-10">
+            <div class="menu_item">
+                <i class="fa fa-home fa-2x"></i>
+            </div>
+
+            <div class="menu_item">
+                <i class="fa fa-users fa-2x"></i>
+            </div>
+
+            <div class="menu_item">
+                <i class="fa fa-list-alt fa-2x" aria-hidden="true"></i>
+            </div>
+
+            <div class="menu_item">
+                <i class="fa fa-clock-o fa-2x" aria-hidden="true"></i>
+            </div>
+        </div>
+        <div class="canvas bit-90">
+            <div class="table">
+                <h1> Tafel 1 </h1>
+            </div>
+        </div>
+    </body>
+</html>
